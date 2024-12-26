f0f2fdf219f4a7097563f33f92555bdabe140504
Ariya Moheb <ariya.mms@gmail.com>
2024-12-26 19:43:44 +0330

    Update min and max count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,7 @@ export class AppComponent {
 
   private getSlapCount() {
     const max = 290;
-    const min = 190;
+    const min = 90;
     return Math.round((Math.random() * (max - min) + min));
   }
 
